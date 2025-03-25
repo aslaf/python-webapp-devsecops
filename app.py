d42de20a9478c9af60b3b96602bb451dc73461e2
Aslaf Sagir <aslafsagir@gmail.com>
2025-03-25 02:22:46 -0400

    Updated .env and app.py

--- app.py
+++ app.py
@@ -13,4 +13,6 @@ def home():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     debug_mode = os.environ.get("DEBUG", "False").lower() == "true"
-    app.run(debug=debug_mode, host="127.0.0.1", port=port)
+    host = os.environ.get(HOST", "0.0.0.0")
+
+    app.run(debug=debug_mode, host=host, port=port)
