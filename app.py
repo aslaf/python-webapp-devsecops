d68c11c97e61c7f1a5a7d54e92e483aba5d0c5cd
Aslaf Sagir <aslafsagir@gmail.com>
2025-03-25 00:47:26 -0400

    Fix B201: Remove hardcoded debug=True and use env variable

--- app.py
+++ app.py
@@ -12,4 +12,5 @@ def home():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    debug_mode = os.environ.get("DEBUG", "False").lower() == "true"
+    app.run(debug=debug_mode, host="0.0.0.0", port=port)
