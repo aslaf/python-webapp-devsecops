bb5849585aeaf24c97ea2f5f802de9a9a49d9c2a
aslaf <60768548+aslaf@users.noreply.github.com>
2025-03-26 04:33:20 -0400

    feat: add basic login page with dashboard route (#9)
    * feat: add basic login page with dashboard route
    
    * feat: add basic login page with dashboard route
    
    * feat: add basic login page with dashboard route
    
    * feat: add basic login page with dashboard route
    
    ---------
    
    Co-authored-by: Aslaf Sagir <aslafsagir@gmail.com>

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
-from flask import Flask
 import os
+from flask import Flask, render_template, request, redirect, url_for
 from dotenv import load_dotenv
-
-load_dotenv() #Load settings from .env file
 
 app = Flask(__name__)
 
-@app.route("/")
+# Just for now (hardcoded login)
+VALID_USERNAME = os.getenv("VALID_USERNAME")
+VALID_PASSWORD = os.getenv("VALID_PASSWORD")
+
+@app.route('/')
 def home():
-    return "Hello, DevSecOps World!"
+    return redirect(url_for('login'))
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    debug_mode = os.environ.get("DEBUG", "False").lower() == "true"
-    host = os.environ.get("HOST", "127.0.0.1")
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
 
-    app.run(debug=debug_mode, host=host, port=port)
+        if username == VALID_USERNAME and password == VALID_PASSWORD:
+            return redirect(url_for('dashboard'))
+        else:
+            return render_template('login.html', error="Invalid credentials")
 
-# testing checks
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+if __name__ == '__main__':
+    debug_mode = os.getenv("FLASK_DEBUG", "False").lower()== "true"
+    app.run(debug=debug_mode)
