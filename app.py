ead98e7d198b3f8768d5fa2debdc9d762fb1d6aa
Aslaf Sagir <aslafsagir@gmail.com>
2025-03-25 21:05:06 -0400

    Avoiding hardcoding 0.0.0.0 to safe 127.0.0.1

--- app.py
+++ app.py
@@ -13,6 +13,6 @@ def home():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     debug_mode = os.environ.get("DEBUG", "False").lower() == "true"
-    host = os.environ.get("HOST", "0.0.0.0")
+    host = os.environ.get("HOST", "127.0.0.1")
 
     app.run(debug=debug_mode, host=host, port=port)
